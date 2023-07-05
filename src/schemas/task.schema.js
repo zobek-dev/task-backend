86940d09ea2bd7fe862ac4532a3fbd8dc8fb66ec
Mediahuella <sebastian@mediahuella.com>
2023-07-05 09:49:25 -0400

    client files added

--- src/schemas/task.schema.js
+++ src/schemas/task.schema.js
@@ -6,6 +6,6 @@ export const createTaskSchema = z.object({
   }),
   description: z.string({
     required_error: 'Description mus be an string'
-  }).optional(),
+  }),
   date: z.string({}).datetime()
 })
