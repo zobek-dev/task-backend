fb907612974cb79004be5af752e79152296dc4a3
Mediahuella <sebastian@mediahuella.com>
2023-07-12 16:32:12 -0400

    register page errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,22 +2,25 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import RegisterPage  from  './pages/RegisterPage'
 import LoginPage from './pages/LoginPage'
+import { AuthProvider } from './context/authContext'
 
 import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<h1>Home Page</h1>}/>
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/register" element={<RegisterPage/>}/>
-        <Route path="/tasks" element={<h1>Tasks</h1>}/>
-        <Route path="/add-taks" element={<h1>Add Task</h1>}/>
-        <Route path="/tasks/:id" element={<h1>Update Task</h1>}/>
-        <Route path="/profile" element={<h1>Profile</h1>}/>
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home Page</h1>}/>
+          <Route path="/login" element={<LoginPage/>}/>
+          <Route path="/register" element={<RegisterPage/>}/>
+          <Route path="/tasks" element={<h1>Tasks</h1>}/>
+          <Route path="/add-taks" element={<h1>Add Task</h1>}/>
+          <Route path="/tasks/:id" element={<h1>Update Task</h1>}/>
+          <Route path="/profile" element={<h1>Profile</h1>}/>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider> 
   )
 }
 
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,36 +1,63 @@
 import { useForm } from 'react-hook-form'
-import { registerRequest } from '../api/auth';
+import { useAuth } from '../context/authContext'
+import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
 
 function RegisterPage(){
 
-  const {register, handleSubmit} = useForm();
+  const { register, handleSubmit, formState: { errors } } = useForm()
+  const { signup, isAuthenticated, errors: registerErrors} = useAuth()
+  const navigate = useNavigate()
+
+  useEffect(()=> {
+    if(isAuthenticated) navigate('/tasks')
+  },[isAuthenticated])
+
+  console.log(registerErrors)
   return(
+    
     <div className="bg-zinc-800 text-white p-10 rounded-md max-w-md mx-auto [&>form>input]:bg-zinc-600 [&>form>input]:placeholder:text-white [&>form>input]:text-white [&>form>input]:rounded-sm [&>form>input]:my-4 [&>form>input]:block [&>form>input]:w-full [&>form>input]:p-2">
       <form onSubmit={handleSubmit(async (values) => {
-        const res = await registerRequest(values)
-        console.log(res)
+        signup(values)
       })}>
+        {
+          registerErrors 
+          && 
+          <div className="bg-red-600 text-white p-2 rounded-sm">{registerErrors.message}</div>
+        }
         <input 
           type="text" 
           {
             ...register('username', 
             {required: true})
           }
+          placeholder="Username"
         />
+        {errors.username && (
+          <p className="text-red-500">Username is required</p>
+        )}
         <input 
           type="email" 
           {
             ...register('email', 
             {required: true})
           }
+          placeholder="Email"
         />
+        {errors.email && (
+          <p className="text-red-500">Email is required</p>
+        )}
         <input
           type="password" 
           {
             ...register('password', 
             {required: true})
           }
+          placeholder="Password"
         />
+        {errors.password && (
+          <p className="text-red-500">Password is required</p>
+        )}
         <button type="submit">
           Submit
         </button>
